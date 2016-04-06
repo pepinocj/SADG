ca5803576e093315466f64090d3bf5279acd9412
Eline <elinevanermen@gmail.com>
2016-04-06 13:43:15 +0200

    logica voor molmode toegevoegd

--- SADG_BackEnd/src/backEnd/ScoreHandler.java
+++ SADG_BackEnd/src/backEnd/ScoreHandler.java
@@ -7,6 +7,14 @@ public class ScoreHandler {
 	}
 
 	public boolean handleScore(GameMode gameMode, String pers1, String pers2, GameState currentState) {
+		return regularMatch(pers1, pers2, currentState); 
+	}
+	
+
+	
+	
+	
+	private boolean regularMatch(String pers1, String pers2, GameState currentState) {
 		boolean isMatch = checkMatch(pers1, pers2, currentState);
 		int secondsPassed = (int) ((System.currentTimeMillis() - currentState.timeStarted) * 60);
 		if(isMatch){
@@ -23,9 +31,34 @@ public class ScoreHandler {
 		return isMatch;
 	}
 	
-	public boolean handleScore(MolMode gameMode, String pers1, String pers2, GameState currentState) {
-		// TODO Auto-generated method stub
-		return false;
+	
+	
+	
+	
+
+	public MatchType handleScore(MolMode gameMode, String pers1, String pers2, GameState currentState) {
+		int secondsPassed = (int) ((System.currentTimeMillis() - currentState.timeStarted) * 60);
+
+		boolean pers1IsMol = currentState.getSongAssignments().get(pers1).equals("MolSong"); //TODO verander naar echte molsongcode
+		boolean pers2IsMol = currentState.getSongAssignments().get(pers2).equals("MolSong");// niet zo mooie code sorry :(
+
+		if(pers1IsMol || pers2IsMol){
+			System.out.println("Successful molmatch!");
+			String mol = "";
+			String victim = "";
+			if(pers1IsMol){ mol = pers1; victim = pers2;}
+			else{ mol = pers2; victim = pers1;}
+			currentState.addToScore(mol, secondsPassed); //TODO eventueel score systeem hier aanpassen
+			currentState.addToScore(victim, -secondsPassed);
+			return MatchType.MOLMATCH;
+
+		}
+		
+		boolean success = regularMatch(pers1, pers2, currentState);
+		if(success){return MatchType.SUCCESS;}
+		else{return MatchType.FAIL;}
+			
+		
 	}
 	
 	
@@ -33,6 +66,9 @@ public class ScoreHandler {
 		// TODO Auto-generated method stub
 		return false;
 	}
+	
+	
+	
 	
 	private boolean checkMatch(String pers1, String pers2, GameState currentState){
 		return (currentState.songAssignments.get(pers1) == (currentState.songAssignments.get(pers2)));
--- SADG_BackEnd/src/backEnd/SongAssigner.java
+++ SADG_BackEnd/src/backEnd/SongAssigner.java
@@ -1,5 +1,6 @@
 package backEnd;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Map;
 
 public class SongAssigner {
@@ -7,31 +8,67 @@ public class SongAssigner {
 	SongPicker songPicker = new SongPicker();
 
 	public Map<String, String> assignSongs(GameMode gameMode, GameState gameState) {
-		int nbOfSongsNeeded = getNbNeeded(gameMode, gameState);
-		ArrayList<String> songs = songPicker.pickSongs(nbOfSongsNeeded, gameState.level, gameState.genre);
-		System.out.println("We picked these songs: " + songs.toString());
-		
+		System.out.println("oei overloading");
 		return null;
 	}
 	
+	public Map<String, String> assignSongs(RegularMode gameMode, GameState gameState) {
+		ArrayList<Person> players = gameState.getPlayers();
+		int nbOfSongsNeeded = players.size()/2;
+		ArrayList<String> songs = songPicker.pickSongs(nbOfSongsNeeded, gameState.level, gameState.genre);
+		System.out.println("We picked these songs: " + songs.toString());
+		songs.addAll(songs); 
+		assert(players.size() == songs.size());
+		Map<String,String> playerSongMap = assignToPlayers(songs, players);
+		return playerSongMap;
+	}
+	
+	//rekent effe op oneven aantal mensen
+	public Map<String, String> assignSongs(MolMode gameMode, GameState gameState) {
+		ArrayList<Person> players = gameState.getPlayers();
 
-	private int getNbNeeded(GameMode gameMode, GameState state){
-		System.out.println("oei oei overloading");
-		return -1;
+		int nbOfSongsNeeded = players.size()/2; 
+		ArrayList<String> songs = songPicker.pickSongs(nbOfSongsNeeded, gameState.level, gameState.genre);
+		System.out.println("We picked these songs: " + songs.toString());
+		songs.addAll(songs); 
+		songs.add("molsong");//TODO dit moet uiteraard veranderen naar de molsongcode fzo
+		assert(players.size() == songs.size());
+		Map<String,String> playerSongMap = assignToPlayers(songs, players);
+		return playerSongMap;	
+	}
+
+	public Map<String, String> assignSongs(TeamMode gameMode, GameState gameState) {
+		ArrayList<Person> players = gameState.getPlayers();
+		int nbOfSongsNeeded = 2;  //rekent op slechts twee teams
+		ArrayList<String> twoSongs = songPicker.pickSongs(nbOfSongsNeeded, gameState.level, gameState.genre);
+		System.out.println("We picked these songs: " + twoSongs.toString());
+		
+		ArrayList<String> songs = new ArrayList<String>();
+		for(int i=0; i<=((players.size()-1)/2); i++){
+			songs.addAll(twoSongs);
+		}
+		
+		assert(players.size() == songs.size() || players.size() +1 == songs.size()); //depends on even of oneven
+		Map<String,String> playerSongMap = assignToPlayers(songs, players);
+		return playerSongMap;	
 	}
 	
-	private int getNbNeeded(RegularMode gameMode, GameState state){
-		return state.getPlayers().size()/2;
+	
+	
+	
+	private Map<String, String> assignToPlayers(ArrayList<String> songs, ArrayList<Person> players) {
+		Map<String,String> result = Collections.<String,String>emptyMap(); 
+		for( Person player : players){
+			int size = songs.size();
+			int random = (int) Math.random()*size;
+			result.put(player.getUserName(),songs.get(random));
+			songs.remove(random);
+		}
+		return result;
 	}
 	
-	private int getNbNeeded(MolMode gameMode, GameState state){
-		//TODO implement
-		return -1;
-	}
+
 	
-	private int getNbNeeded(TeamMode gameMode, GameState state){
-		return -1;
-		//TODO implement
-	}
+	
 	
 }
