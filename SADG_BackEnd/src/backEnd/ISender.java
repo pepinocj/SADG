289372e936055c83d2b1dd497ef587f90ea74437
KarenRaets <karen.raets@student.kuleuven.be>
2016-05-04 14:28:13 +0200

    throws IO exceptions sender weg

--- SADG_BackEnd/src/backEnd/ISender.java
+++ SADG_BackEnd/src/backEnd/ISender.java
@@ -5,39 +5,34 @@ import java.util.Map;
 
 
 public interface ISender {
-	
-	//TODO in receiver een addplayer? en een removeplayer en er zo een mini interface moeten zijn om genre 
-	//en difficulty te kiezen (en score op te tonen), maar das voor later
-	
 	// Zend de link/index van de te spelen muziek naar de juiste gebruiker.
 	// Gebruikers zijn al eerder doorgegeven in initiateRound(..). Link kan in de vorm van een
 	// string zijn
-	public abstract void sendMusic(Map<String,Integer> music) throws IOException;
+	public abstract void sendMusic(Map<String,Integer> music);
 	
 	// Starten van een ronde aka start spelen van muziek over x aantal seconden. 
 	//TODO uittesten of dat dan effectief tegelijk begint, anders absoluut tijdstip gebruiken
-	public abstract void startRound(long timeToStart) throws IOException;
-	
-	
-	public abstract void sendSystemTime(long systemTime) throws IOException;
-	
+	public abstract void startRound(long timeToStart);
+		
 	//Als laatste nodige match is gevonden (vb na eerste drie matchen stop), zend een
 	// signaal naar alle spelers dat deze ronde stopt, aka laat de muziek stoppen, scorebord
 	// komt tevoorschijn?
 	public abstract void stopRound() throws IOException;
 	
 	//Winner van de ronde announcen
-	public abstract void announceWinner(String person) throws IOException;
+	public abstract void announceWinner(String person);
 	
-	public abstract void reportMolVerification(String mol, String victim) throws IOException;
+	public abstract void reportMolVerification(String mol, String victim);
 	
-	public abstract void reportVerification(String id1, String id2, boolean stateOfSuccess) throws IOException;
+	public abstract void reportVerification(String id1, String id2, boolean stateOfSuccess);
 	
-	public abstract void chooseNewName(String id) throws IOException;
+	public abstract void chooseNewName(String id);
 	
 	//Eenmaal spelen gedaan is: sluiten van communicatie
 	public abstract void closeCommunication();
 	
 	//Lelijke fix voor consistentie
 	public abstract void removeUser(String name);
+
+	public void sendScoreBoard(Map<String, Integer> scores);
 }
